ef44cb5dae2235c1337475c02a8876a9669642b7
Duncan Staats <staatsd@wlhs.wlwv.k12.or.us>
2024-11-06 17:49:06 +0000

    new4

--- Begining.py
+++ Begining.py
@@ -18,67 +18,78 @@ ____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
 /______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
 ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/______/_'''
-
-print(intro_art)
-print("You gaze upon what will be your last job.\nYou stare with such intensity that you don't even notice...")
-print("Thud!")
-print("Thump, Thump, Thump.\nYou wake up! You have a major headache and find out you are in chains.")
-print("Groggly, you think of what to do as you are stuck on a ship.\n1. Will you scream for help and pray some one will hear you.\n2. Will you stay quiet and try to unchain yourself.")
-
 while True:
-    choice_1 = int(input("1 or 2: "))
+    print(intro_art)
+    print("You gaze upon what will be your last job.\nYou stare with such intensity that you don't even notice...")
+    print("Thud!")
+    print("Thump, Thump, Thump.\nYou wake up! You have a major headache and find out you are in chains.")
+    print("Groggly, you think of what to do as you are stuck on a ship.\n1. Will you scream for help and pray some one will hear you.\n2. Will you stay quiet and try to unchain yourself.")
 
-    if choice_1 == 1:
-        print("He"+"e"*6+"l"*7+"p")
-        print("A man, from the shadows, takes a hand that taste of grim and salt to your mouth.")
-        print("S"+"hhh"+"u"*11+".","You'll wake the guards, are you trying to be whipped boy.")
-        break
+    while True:
+        choice_1 = int(input("1 or 2: "))
 
-    elif choice_1 == 2:
-        print("You move your chained hand to your mouth to produce a pick.")
-        print("You rattle with your chains using the pick and with a Clink! Your hands are set free")
-        print("Before you finish a man sweeps out of the shaddows and tackles you to get your pick.\nYou loose your grip and the man takes the needle with him back into the shadows of the room.")
-        break
+        if choice_1 == 1:
+            print("He"+"e"*6+"l"*7+"p")
+            print("A man, from the shadows, takes a hand that taste of grim and salt to your mouth.")
+            print("S"+"hhh"+"u"*11+".","You'll wake the guards, are you trying to be whipped boy.")
+            break
 
-    else:
-        print("You have decided to answer poorly, you get stabbed by another while chained and bleed to death.")
-        print("You have died and will now be returned to you first choice. Please answer correctly next time.")
+        elif choice_1 == 2:
+            print("You move your chained hand to your mouth to produce a pick.")
+            print("You rattle with your chains using the pick and with a Clink! Your hands are set free")
+            print("Before you finish a man sweeps out of the shaddows and tackles you to get your pick.\nYou loose your grip and the man takes the needle with him back into the shadows of the room.")
+            break
 
-print()
+        else:
+            print("You have decided to answer poorly, you get stabbed by another while chained and bleed to death.")
+            print("You have died and will now be returned to you first choice. Please answer correctly next time.")
+            continue
 
-print("Through your struggles with the other inmate you develop a bond, a bond to escape together and see the light of day.")
-print("You devise a plan to sneek out at night and gather supplie from the ship for your row home, what will you do:")
-print("1. Check out the captain quarters and the helm.\n2. Check out the crews sleeping quarters.\n3. Check out the cafeteria and armory.")
+    print()
 
-while True:
-    choice_2 = int(input("1, 2, or 3: "))
+    print("Through your struggles with the other inmate you develop a bond, a bond to escape together and see the light of day.")
+    print("You devise a plan to sneek out at night and gather supplie from the ship for your row home, what will you do:")
+    print("1. Check out the captain quarters and the helm.\n2. Check out the crews sleeping quarters.\n3. Check out the cafeteria and armory.")
+
+    while True:
+        choice_2 = int(input("1, 2, or 3: "))
+
+        if choice_2 == 1:
+            print("You and your friend slowly make your way to the captains door.\nYou try and open the door and relize it is locked and move your way up to the helm. ")
+            print("You step on a creaky floor board and wake the captain.\nThe captain yells and wakes everyone up and you and your friend die to the captain and the crew.")
+            break
+
+        elif choice_2 == 2:
+            print("You creep your way to the crew quaters and have your friend watch the door.")
+            print("You look around and fine nothing within the crew quaters and decide to leave before getting caught.")
+            break
+
+        elif choice_2 == 3:
+            print("You find a ton of supplies in the armory and cafeteria and now you have to choose what to bring:")
+            print("1. Will you grab stale bread and a small knife, and a small canteen of water.\n2. Will you grab some jerky and bread along with a blunderbuss pistol and a cutlass and a medium size canteen of water")
+            sub_choice_1 = int(input("1 or 2: "))
+            break
+
+        else:
+            print("You have decided to answer poorly, you get shot by the captian and bleed to death.")
+            print("You have died and will now be returned to you last choice. Please answer correctly next time.")
+            continue
 
     if choice_2 == 1:
-        print("You and your friend slowly make your way to the captains door.\nYou try and open the door and relize it is locked and move your way up to the helm. ")
-        print("You step on a creaky floor board and wake the captain.\nThe captain yells and wakes everyone up and you and your friend die to the captain and the crew.")
-        break
+        print("You have died and will now be returned to the start.")
+        continue
 
     elif choice_2 == 2:
-        print("You creep your way to the crew quaters and have your friend watch the door.")
-        print("You look around and fine nothing within the crew quaters and decide to leave before getting caught.")
+        print("You sneak on to the rowboat peacefully without alerting the guards.")
         break
 
-    elif choice_2 == 3:
-        print("You find a ton of supplies in the armory and cafeteria and now you have to choose what to bring:")
-        print("1. Will you grab stale bread and a small knife, and a small canteen of water.\n2. Will you grab some jerky and bread along with a blunderbuss pistol and a cutlass and a medium size canteen of water")
-        sub_choice_1 = int(input("1 or 2: "))
+    elif sub_choice_1 == 1:
+        print("You sneak on to the rowboat peacefully without alerting the guards.")
         break
 
-    else:
-        print("You have decided to answer poorly, you get shot by the captian and bleed to death.")
-        print("You have died and will now be returned to you last choice. Please answer correctly next time.")
-
-if choice_2 == 2:
-        print("You sneak on to the rowboat peacefully without alerting the guards.")
-
-elif sub_choice_1 == 1:
-        print("You sneak on to the rowboat peacefully without alerting the guards.")
-
-elif sub_choice_1 == 2:
+    elif sub_choice_1 == 2:
         print("You drop the pistol and it fires waking up the crew and captain.")
         print("You and your friend die in this process and do not make it out alive.")
+        continue
+
+    print()
